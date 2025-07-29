4f8730037052b2b19ad256b36a8eebc666f8b9f7
NourAshour <nashour767@gmail.com>
2025-07-29 21:16:35 +0300

    Arithmetic Operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            result = num1 + num2
+            return result
+        case 'subtract':
+            result = num1 - num2
+            return result
+        case 'multiply':
+            result = num1 * num2
+            return result
+        case 'divide':
+            if num2 != 0:
+                result = num1 / num2
+                return result
+            else:
+                print("You can't divide by zero")
+        case _:
+            print("Enter a valid operation")
+    
