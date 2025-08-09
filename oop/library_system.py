79b1c11a3ed635cb59a718c197b0f6593d83eb1e
NourAshour <nashour767@gmail.com>
2025-08-09 22:27:34 +0300

    Library

--- oop/library_system.py
+++ oop/library_system.py
@@ -20,8 +20,8 @@ class PrintBook(Book):
         return f"Print{super().__str__()}, Page Count: {self.page_count}"
 
 class Library:
-    def __init__(self, books = []):
-        self.books = books
+    def __init__(self, books):
+        self.books = []
 
     def add_book(self, book):
         self.books.append(book)
